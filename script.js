b242641d8e8960d231e8c921b087e5f56bd19e30
Baguette-Inu <baguetteinu@gmail.com>
2025-01-28 04:37:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,117 @@
 // Memes by city
 const cityMemes = {
   paris: [
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/LANDING.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/AIRPORT.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/subway.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/shopping.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/AQUARIUM.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/COFFEE.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/LADEFENSE.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/asterix.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/disney.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/baguetteinthemouth.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/bench.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/catacombe1.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/champeelysee.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/croisiererealistic.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/cuteundereiffeltower.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/dinercouchersol.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/fashionweek.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/library.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/livreurbaguette.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/louvres.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/macaron.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/manegeeiffeltower.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/moulinrouge.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/moulinrouge2.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/musician.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/nightlife.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/peintrefriend.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/pigeon.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/piquenique.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/pont.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/relax.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/shibacuistot.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/shibapeintre.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/shopping.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/standingbaguette.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/boshibaguette.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/boshibaguettecasinogain.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/baguettespacecraft.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/baguettetothemoon.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/standingonthemoonwithbaguetteonthehand.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/plantingbaguetteonthemoon.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/1meeting1.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/2dinerauchandelle.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/3WALKINGTOGETHER.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/4walkromanticmontmartre.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/5BENCHLOVE.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/6REGARDINTIME.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/8RENCARD.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/9diner.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/10soir%C3%A9e.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/11demande.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/12eglise.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/13gondole.webp',
-    'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/14honeymoondepart.webp',
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/LANDING.webp',
+      description: 'Shibaguette Inu lands in Paris, greeted by the breathtaking view of the Eiffel Tower. A new journey begins!'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/AIRPORT.webp',
+      description: 'At the airport, excitement fills the air as Shibaguette prepares to explore the City of Lights.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/subway.webp',
+      description: 'Navigating the bustling Paris Metro, Shibaguette enjoys the pulse of the city underground.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/shopping.webp',
+      description: 'A stop on the famous Rue de Rivoli, Shibaguette explores chic Parisian boutiques.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/AQUARIUM.webp',
+      description: 'A magical visit to the Aquarium de Paris, where vibrant marine life adds wonder to the journey.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/COFFEE.webp',
+      description: 'A pause at a cozy café, enjoying the quintessential Parisian espresso experience.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/LADEFENSE.webp',
+      description: 'In La Défense, Shibaguette admires the sleek modernity of Paris, blending tradition with innovation.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/asterix.webp',
+      description: 'A day of adventure at Parc Astérix, filled with thrilling rides and playful moments.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/disney.webp',
+      description: 'Magic fills the air at Disneyland Paris, where dreams come to life under glowing lights.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/baguetteinthemouth.webp',
+      description: 'Shibaguette proudly carries his favorite baguette through the streets of Paris.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/bench.webp',
+      description: 'A quiet moment of reflection on a park bench, with the Eiffel Tower in the background.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/catacombe1.webp',
+      description: 'Exploring the eerie Catacombs of Paris, Shibaguette faces the city’s hidden mysteries.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/champeelysee.webp',
+      description: 'Walking down the iconic Champs-Élysées, Shibaguette feels the grandeur of Paris.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/croisiererealistic.webp',
+      description: 'A serene cruise on the Seine, soaking in the romance of Paris at sunset.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/cuteundereiffeltower.webp',
+      description: 'Under the Eiffel Tower, Shibaguette finds the beauty of Paris magnified by love and charm.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/dinercouchersol.webp',
+      description: 'A candlelit dinner by the Seine, the perfect Parisian evening unfolds.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/fashionweek.webp',
+      description: 'Exploring Paris during Fashion Week, Shibaguette celebrates the city’s chic vibe.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/library.webp',
+      description: 'Shibaguette explores the grand library of Paris, immersing himself in the city’s rich history and stories.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/livreurbaguette.webp',
+      description: 'Delivering baguettes across the city, Shibaguette adds a personal touch to every loaf.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/louvres.webp',
+      description: 'A day at the Louvre, marveling at iconic art and the timeless beauty of Parisian culture.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/macaron.webp',
+      description: 'Treating himself to colorful macarons, Shibaguette savors the sweet essence of Paris.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/manegeeiffeltower.webp',
+      description: 'A joyful ride on the carousel near the Eiffel Tower, bringing childhood wonder to the forefront.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/moulinrouge.webp',
+      description: 'An evening at the Moulin Rouge, dazzled by the bright lights and vibrant performances.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/1meeting1.webp',
+      description: 'Their first meeting near the Eiffel Tower was nothing short of magical. Sparks flew as their eyes met under the Parisian lights.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/2dinerauchandelle.webp',
+      description: 'A candlelit dinner by the Seine, where they shared laughter, stories, and their love for baguettes and croissants.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/13gondole.webp',
+      description: 'On their honeymoon, they enjoyed a serene gondola ride on the Seine, the Eiffel Tower glowing in the moonlight.'
+    },
+    { 
+      url: 'https://raw.githubusercontent.com/Baguette-Inu/Baguette-inu/refs/heads/main/14honeymoondepart.webp',
+      description: 'Departing for their honeymoon adventure across France, their love story continues to inspire.'
+    },
   ],
+};
+
   lyon: [],
   marseille: [],
   nice: [],
